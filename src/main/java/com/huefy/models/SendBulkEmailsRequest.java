9c916efe69fb2c734a99265c068a9c6827a99c35
Kingsley Eze <firebox@ikingsley.com>
2026-04-24 20:41:55 +0200

    refact(java): simplify SendBulkEmailsRequest to templateKey, recipients, provider

--- src/main/java/com/huefy/models/SendBulkEmailsRequest.java
+++ src/main/java/com/huefy/models/SendBulkEmailsRequest.java
@@ -5,9 +5,9 @@ import java.util.List;
 public record SendBulkEmailsRequest(
     String templateKey,
     List<BulkRecipient> recipients,
-    String fromEmail,
-    String fromName,
-    String providerType,
-    Integer batchSize,
-    String correlationId
-) {}
+    EmailProvider provider
+) {
+    public SendBulkEmailsRequest(String templateKey, List<BulkRecipient> recipients) {
+        this(templateKey, recipients, null);
+    }
+}
